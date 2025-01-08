6cbd7c34c504c64ce8ff8bdeb00e9d01b63c3824
Hieronimus Pradnya Narendra <hieropradnya77@gmail.com>
2025-01-08 15:04:44 +0700

    add price detail model and migration

---
+++ database/migrations/2025_01_08_080044_create_price_details_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('price_details', function (Blueprint $table) {
+            $table->id();
+            $table->integer('product_subtotal');
+            $table->integer('shipping_cost');
+            $table->integer('service_fee');
+            $table->integer('discount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('price_details');
+    }
+};
